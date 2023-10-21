6c2dbd4c17512a6acca00021cfebf5f8f8588d79
Lauren Kouri <ljk39@uakron.edu>
2023-10-21 10:43:38 -0400

    Fix function names

--- frontend/src/Components/user_registration.js
+++ frontend/src/Components/user_registration.js
@@ -1,88 +1,86 @@
-function UserRegistration() {
-    function RegistrationForm() {
-        const [formData, setFormData] = useState({
-            // matches the SQL query data for users table
-            userName: '',
-            password: '',
-            locationCountry: '',
-            locationState: '',
-            locationCity: '',
+
+function RegistrationForm() {
+    const [formData, setFormData] = useState({
+        // matches the SQL query data for users table
+        userName: '',
+        password: '',
+        locationCountry: '',
+        locationState: '',
+        locationCity: '',
+    });
+}
+
+const handleSubmit = (e) => {
+    // extracts name and value from user input
+    const { name, value } = e.target;
+    // updates formData with the new data
+    setFormData({ ...formData, [name]: value });
+}
+
+const handleRegistration = async (e) => {
+    // stop page from refreshing on submission
+    e.preventDefault();
+    try {
+        const response = await fetch('/register', {
+        // specifies the data should be sent to the server
+        method: 'POST',
+        headers: {
+            // sets content of the request to json
+            'Content-Type': 'application/json',
+        },
+        // convert data in formData to json string
+        body: JSON.stringify(formData),
         });
-    }
-
-    handleSubmit = (e) => {
-        // extracts name and value from user input
-        const { name, value } = e.target;
-        // updates formData with the new data
-        setFormData({ ...formData, [name]: value });
-    }
-
-    const handleRegistration = async (e) => {
-        // stop page from refreshing on submission
-        e.preventDefault();
-        try {
-            const response = await fetch('/register', {
-            // specifies the data should be sent to the server
-            method: 'POST',
-            headers: {
-                // sets content of the request to json
-                'Content-Type': 'application/json',
-            },
-            // convert data in formData to json string
-            body: JSON.stringify(formData),
-            });
-            // tests if submission was successful
-            if (response.ok) {
-            console.log('Registration successful');
-            } else {
-            console.error('Registration failed');
-            }
-        } catch (error) {
-            console.error('Error:', error);
+        // tests if submission was successful
+        if (response.ok) {
+        console.log('Registration successful');
+        } else {
+        console.error('Registration failed');
         }
-    };
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
 
-    return(
-        <form onSubmit={handleSubmit}>
-        <input
-            type="text"
-            name="username"
-            placeholder="Username"
-            value={formData.userName}
-            onChange={handleInputChange}
-        />
-        <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={formData.password}
-            onChange={handleInputChange}
-        />
-        <input
-            type="text"
-            name="locationcity"
-            placeholder="City"
-            value={formData.locationCity}
-            onChange={handleInputChange}
-        />
-        <input
-            type="text"
-            name="locationstate"
-            placeholder="State"
-            value={formData.locationState}
-            onChange={handleInputChange}
-        />
-        <input
-            type="text"
-            name="locationcountry"
-            placeholder="Country"
-            value={formData.locationCountry}
-            onChange={handleInputChange}
-        />
-        <button type="submit">Register Account</button>
-        </form>
-    );
+return(
+    <form onSubmit={handleRegistration}>
+    <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        value={formData.userName}
+        onChange={handleSubmit}
+    />
+    <input
+        type="password"
+        name="password"
+        placeholder="Password"
+        value={formData.password}
+        onChange={handleSubmit}
+    />
+    <input
+        type="text"
+        name="locationcity"
+        placeholder="City"
+        value={formData.locationCity}
+        onChange={handleSubmit}
+    />
+    <input
+        type="text"
+        name="locationstate"
+        placeholder="State"
+        value={formData.locationState}
+        onChange={handleSubmit}
+    />
+    <input
+        type="text"
+        name="locationcountry"
+        placeholder="Country"
+        value={formData.locationCountry}
+        onChange={handleSubmit}
+    />
+    <button type="submit">Register Account</button>
+    </form>
+);
 
-    };
-
-export default UserRegistration()
+export default RegistrationForm()
